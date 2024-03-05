f5485c0ca9d07db8d595c91daba4e032affe7a09
m9rk1sv33n <funtimes@markisveen.club>
2024-03-05 10:27:21 +0100

    add catch block and error messaging

--- src/components/FeedbackList.tsx
+++ src/components/FeedbackList.tsx
@@ -1,19 +1,31 @@
 import { useEffect, useState } from "react";
 import FeedbackItem from "./FeedbackItem";
 import Spinner from "./Spinner";
+import ErrorMessage from "./ErrorMessage";
 
 export default function FeedbackList() {
   const [feedbackItems, setFeedbackItems] = useState([]);
   const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState("");
 
   useEffect(() => {
     setIsLoading(true);
     fetch(
-      "https://bytegrad.com/course-assets/projects/corpcomment/api/feedbacks"
+      "https://bytegrad.com/course-assets/projects/corpcomment/api/feedddbacks"
     )
-      .then((response) => response.json())
+      .then((response) => {
+        if (!response.ok) {
+          throw new Error("Failed to fetch feedbacks");
+        }
+        return response.json();
+      })
       .then((data) => {
         setFeedbackItems(data.feedbacks);
+        setIsLoading(false);
+      })
+      .catch(() => {
+        //network error, parse error, or response is not ok
+        setError("Something went wrong.");
         setIsLoading(false);
       });
   }, []);
@@ -21,6 +33,7 @@ export default function FeedbackList() {
   return (
     <ol className="feedback-list">
       {isLoading ? <Spinner /> : null}
+      {error ? <ErrorMessage message={error} /> : null}
       {feedbackItems.map((feedbackItem) => (
         <FeedbackItem key={feedbackItem.id} feedbackItem={feedbackItem} />
       ))}
